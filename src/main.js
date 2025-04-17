3ccc88b3dda7d1a9bc0fc16b3fd1c1c056d2b4b8
panzer224 <saberklug@gmail.com>
2025-04-17 12:40:59 +0800

    firebase hosting

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import "./assets/tailwind.css";
 import { createApp } from "vue";
+import { createPinia } from "pinia"; // Add this import
 import App from "./App.vue";
 import router from "./router";
 import { auth } from "./firebase-config";
 import { registerSW } from "./registerServiceWorker";
 
+// Create pinia instance
+const pinia = createPinia();
+
 let app;
 
 auth.onAuthStateChanged(() => {
   if (!app) {
-    app = createApp(App).use(router).mount("#app");
+    app = createApp(App).use(pinia).use(router).mount("#app");
   }
 });
 
